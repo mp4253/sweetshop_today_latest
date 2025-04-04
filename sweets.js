592c00116822817fe401608d3c2e53b630b0f32d
mp4253 <111766615+mp4253@users.noreply.github.com>
2025-04-04 12:29:24 +0530

    sweets.js

--- sweets.js
+++ sweets.js
@@ -1,15 +1,9 @@
 function toggleMenu() {
     var menu = document.getElementById("menu_list");
-    var content = document.getElementById("content");
-    
-    menu.classList.toggle("show");
-    
-    if (menu.classList.contains("show")) {
-        content.classList.add("shifted");
-    } 
-    
-    else {
-        
-        content.classList.remove("shifted");
+
+    if (menu) {
+        menu.classList.toggle("show");
+    } else {
+        console.error("Element with ID 'menu_list' not found.");
     }
 }
